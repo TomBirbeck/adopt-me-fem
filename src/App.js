8b57542cc1b04702088ba1fe41f99fcdfbc88f3f
TomBirbeck <tombirbeck47@gmail.com>
2022-11-17 14:14:54 +0000

    added basic search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams.js';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name={'Destiny'} animal={'Cat'} breed={'Short-hair'} />
-      <Pet name={'Sherlock'} animal={'Dog'} breed={'Shi-tzu'} />
-      <Pet name={'Steve'} animal={'Frog'} breed={'Bull'} />
+      <SearchParams />
     </div>
   );
 };

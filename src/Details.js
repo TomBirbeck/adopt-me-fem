589f408e7d9bc81895c231bcca3c2a7ac75d4231
TomBirbeck <tombirbeck47@gmail.com>
2022-11-18 13:26:53 +0000

    added error boundary to the details page

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { Component } from 'react';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends Component {
   //   constructor(props) {
@@ -51,7 +52,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />;
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,40 @@
+//error boundaries must be class components
+import { Component } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+
+class ErrorBoundary extends Component {
+  state = { hasError: falsem, redirect: false };
+  static getDerivedStateFromErro() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // would place your client side error catcher here
+    console.error(error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ redirect: true });
+      }, 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to='/' />;
+    } else if (this.state.hasError) {
+      return (
+        <h2>
+          There was an error.
+          <Link to='/'>Click here</Link> to go back to the homepage or wait 5
+          seconds and we will do if for you.
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

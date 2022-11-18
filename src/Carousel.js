7d5599f3b84463213d85272e7fe13202dc40d81e
TomBirbeck <tombirbeck47@gmail.com>
2022-11-18 07:36:02 +0000

    added handleclickindex function to switch between images for the carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ['https://pets-images.dev-apis.com/pets/none.jpg'],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -19,8 +25,11 @@ class Carousel extends Component {
         <div className='carousel-smaller'>
           {images.map((photo, index) => (
             <img
+              // for accessibility you would wrap the image in something that can be picked up by tools eg/ <a></a>
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={index}
               className={index === active ? 'active' : ''}
               alt='animal thumbnail'
             />
